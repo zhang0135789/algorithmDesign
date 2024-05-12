0a1ce2f51a3377791be8e3c4c59e0b5a9551594e
zhang0135789 <zz634682577@163.com>
2024-05-12 17:56:45 +0800

    插入排序 希尔排序

---
+++ src/main/java/com/demo01直接插入排序/insertionSort.java
@@ -0,0 +1,39 @@
+package com.demo01直接插入排序;
+
+import java.util.Arrays;
+
+/**
+ * @Author: zz
+ * @Description:
+ * @Date: 17:26 2024/05/12 0012
+ * @Modified By
+ */
+public class insertionSort {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 4, 3, 2, 6, 5, 8, 7, 9};
+        System.out.println("排序前" + Arrays.toString(nums));
+        insertionSort.insertionSort(nums);
+        System.out.println("排序后"+Arrays.toString(nums));
+    }
+
+    public static void insertionSort(int[] nums) {
+        // 首选记录数组的长度
+        int len = nums.length;
+        // 从数组的第二个元素开始
+        for (int i = 1; i< len; i++) {
+            // 依次取出一个元素，存储在current变量中
+            Integer current = nums[i];
+            // 定义一个指针，初始化当前元素的前一个位置
+            int j = i - 1;
+            // 重复上述操作，直到找到一个位置j，使得 nums[j] <= current，或者 j 已经到达数组的起始位置。
+            while (j >= 0 && nums[j] > current) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            nums[j + 1] = current;
+        }
+    }
+
+
+}
---
+++ src/main/java/com/demo02希尔排序/shellSort.java
@@ -0,0 +1,51 @@
+package com.demo02希尔排序;
+
+import com.demo01直接插入排序.insertionSort;
+
+import java.util.Arrays;
+
+/**
+ * @Author: zz
+ * @Description:
+ * @Date: 17:43 2024/05/12 0012
+ * @Modified By
+ */
+public class shellSort {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 4, 3, 2, 6, 5, 8, 7, 9};
+        System.out.println("排序前" + Arrays.toString(nums));
+        shellSort.shellSort(nums);
+        System.out.println("排序后"+Arrays.toString(nums));
+    }
+
+
+    public static void shellSort(int[] arr) {
+        //交换的次数
+        int num = 0;
+        int n = arr.length;
+        // 初始化增量gap为数组长度的一半
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            System.out.println("gap值" + gap);
+            // 对于每个增量gap，进行一次插入排序
+            for (int i = gap; i < n; i++) {
+                //temp表示当前操作的元素
+                int temp = arr[i];
+                //j是一个指针，表示当前操作的元素
+                int j = i;
+                // 对于每个元素，向前比较gap个距离的元素。如果前面的元素大于当前元素才需要交换
+                while (j >= gap && arr[j - gap] > temp) {
+                    //后面元素挪到前面
+                    arr[j] = arr[j - gap];
+                    //当前操作元素变为前gap个
+                    j -= gap;
+                }
+                //将本次需要操作元素放到移动的位置
+                arr[j] = temp;
+                num++;
+                System.out.println(gap + Arrays.toString(arr));
+            }
+        }
+        System.out.println("一共交换了："+ num +"次");
+    }
+}
